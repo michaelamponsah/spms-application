0285d9fa12b6c39c6c1a5473d4e2f3cd63795208
Michael Amponsah <amponsahmichaelkwame@gmail.com>
2022-05-26 12:43:04 +0000

    Dashboard layout implementaion
    Wrote a mockup code for dashboard. Yet to implement a full dashboard layout

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SideNavigation from "../../components/SideNavigation";
+import PromotionRecordsPage from "../PromotionRecords";
 import classes from "./style.module.css";
 
 const Dashboard = () => {
   return (
     <section className={classes.dashboard}>
       <SideNavigation />
+      <PromotionRecordsPage />
     </section>
   );
 };
--- src/pages/PromotionRecords/index.js
+++ src/pages/PromotionRecords/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import classes from "./style.module.css";
 
 const PromotionRecordsPage = () => {
   return (
-    <div>PromotionRecordsPage</div>
-  )
-}
+    <div>
+      <h2 className={classes.title}>Lecturer Promotion </h2>
+      <div className={classes["content__area"]}>
+      </div>
+    </div>
+  );
+};
 
-export default PromotionRecordsPage
+export default PromotionRecordsPage;
